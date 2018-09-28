af00d25bc44f469be53a1435e93980fbbd84c102
Alan Vardy <alanvardy@gmail.com>
2018-09-28 17:40:26 -0600

    rearranged layout file

--- app.rb
+++ app.rb
@@ -1,32 +1,32 @@
-require "sinatra"
+require 'sinatra'
 
 class App < Sinatra::Base
-  get "/" do
-    @title = "Welcome"
-    erb :"en/index", :layout => :"en/layout"
+  get '/' do
+    @title = 'CorneliaKelinske.com'
+    erb :"en/index", layout: :"layouts/en"
   end
-  get "/aboutme" do
-    @title = "About Me"
-    erb :"en/aboutme", :layout => :"en/layout"
+  get '/aboutme' do
+    @title = 'CK.com - About Me'
+    erb :"en/aboutme", layout: :"layouts/en"
   end
-  get "/services" do
-    @title = "Services"
-    erb :"en/services", :layout => :"en/layout"
+  get '/services' do
+    @title = 'CK.com - Services'
+    erb :"en/services", layout: :"layouts/en"
   end
-  get "/translation" do
-    @title = "Translation"
-    erb :"en/translation", :layout => :"en/layout"
+  get '/translation' do
+    @title = 'CK.com - Translation'
+    erb :"en/translation", layout: :"layouts/en"
   end
-  get "/interpretation" do
-    @title = "Interpretation"
-    erb :"en/interpretation", :layout => :"en/layout"
+  get '/interpretation' do
+    @title = 'CK.com - Interpretation'
+    erb :"en/interpretation", layout: :"layouts/en"
   end
-  get "/feedback" do
-    @title = "Feedback"
-    erb :"en/feedback", :layout => :"en/layout"
+  get '/feedback' do
+    @title = 'CK.com - Feedback'
+    erb :"en/feedback", layout: :"layouts/en"
   end
-  get "/contact" do
-    @title = "Contact"
-    erb :"en/contact", :layout => :"en/layout"
+  get '/contact' do
+    @title = 'CK.com - Contact'
+    erb :"en/contact", layout: :"layouts/en"
   end
 end
