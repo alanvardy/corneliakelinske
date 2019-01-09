f4c52326519af46d01ed7d92f7752e96cb11ccfa
Alan Vardy <alanvardy@gmail.com>
2019-01-09 15:47:54 -0700

    client changes

--- test/controllers/de_controller_test.rb
+++ test/controllers/de_controller_test.rb
@@ -1,34 +1,45 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class DeControllerTest < ActionDispatch::IntegrationTest
-  test "should get ubermich" do
+  test 'should get ubermich' do
     get de_ubermich_url
     assert_response :success
   end
 
-  test "should get leistungen" do
+  test 'should get leistungen' do
     get de_leistungen_url
     assert_response :success
   end
 
-  test "should get feedback" do
+  test 'should get feedback' do
     get de_feedback_url
     assert_response :success
   end
 
-  test "should get kontakt" do
+  test 'should get kontakt' do
     get de_kontakt_url
     assert_response :success
   end
 
-  test "should get ubersetzen" do
+  test 'should get ubersetzen' do
     get de_ubersetzen_url
     assert_response :success
   end
 
-  test "should get dolmetschen" do
+  test 'should get dolmetschen' do
     get de_dolmetschen_url
     assert_response :success
   end
 
+  test 'should get index' do
+    get de_index_url
+    assert_response :success
+  end
+
+  test 'should get linzenzen' do
+    get de_linzenzen_url
+    assert_response :success
+  end
 end
--- test/controllers/en_controller_test.rb
+++ test/controllers/en_controller_test.rb
@@ -1,44 +1,50 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class EnControllerTest < ActionDispatch::IntegrationTest
-  test "should get aboutme" do
+  test 'should get aboutme' do
     get en_aboutme_url
     assert_response :success
   end
 
-  test "should get contact" do
+  test 'should get contact' do
     get en_contact_url
     assert_response :success
   end
 
-  test "should get feedback" do
+  test 'should get feedback' do
     get en_feedback_url
     assert_response :success
   end
 
-  test "should get index" do
+  test 'should get index' do
     get en_index_url
     assert_response :success
   end
 
-  test "should get interpretation" do
+  test 'should get interpretation' do
     get en_interpretation_url
     assert_response :success
   end
 
-  test "should get resume" do
+  test 'should get resume' do
     get en_resume_url
     assert_response :success
   end
 
-  test "should get services" do
+  test 'should get services' do
     get en_services_url
     assert_response :success
   end
 
-  test "should get translation" do
+  test 'should get translation' do
     get en_translation_url
     assert_response :success
   end
 
+  test 'should get licenses' do
+    get en_licenses_url
+    assert_response :success
+  end
 end
