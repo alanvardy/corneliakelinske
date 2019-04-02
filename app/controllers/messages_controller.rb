570ef6f159ae6fc88db9aad3e7bd45bf6653e41e
Alan Vardy <alanvardy@gmail.com>
2019-04-02 12:25:35 -0600

    Contact form

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+    @lang = params['lang']
+  end
+
+  def create
+    @message = Message.new message_params
+    if @message.valid?
+      @message.send
+      redirect_to root_path
+      flash[:success] = 'I have received your message and will be in touch soon!'
+    else
+      flash[:warning] = 'There was an error sending your message. Please try again.'
+      render :new
+        end
+    end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:name, :email, :phone_number, :body)
+    end
+end
